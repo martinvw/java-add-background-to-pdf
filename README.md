e469bf5d4012a172c70af1967bacad33b3596f11
Martin van Wingerden <martin@martinvw.nl>
2023-12-01 15:54:25 +0100

    Second version

---
+++ README.md
@@ -0,0 +1,3 @@
+For years I was looking for a proper tool to add an image background to my Excel invoices, converted to a PDF.
+
+In the end developing it was the fastest way.
--- src/main/java/nl/martinvw/pdfbackground/Main.java
+++ src/main/java/nl/martinvw/pdfbackground/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package nl.martinvw.pdfbackground;
 
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.geom.PageSize;
@@ -8,31 +8,57 @@ import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Image;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
-
+    private static final PageSize PAGE_SIZE = PageSize.A4;
 
     public static void main(String[] args) throws IOException {
-        byte[] backgroundBytes = Main.class.getClassLoader().getResourceAsStream("2020-briefpapier.png").readAllBytes();
+        byte[] backgroundBytes = loadBackground();
 
         String inputFilename = args[0];
-        String outputFilename = inputFilename.replace(".pdf", "-bg.pdf");
-        try (PdfReader reader = new PdfReader(inputFilename); PdfWriter writer = new PdfWriter(inputFilename)) {
+        String outputFilename = getOutputFilename(args, inputFilename);
+
+        try (PdfReader reader = new PdfReader(inputFilename); PdfWriter writer = new PdfWriter(outputFilename)) {
 
             PdfDocument pdfDocument = new PdfDocument(reader, writer);
-            PageSize pageSize = PageSize.A4;
-            Document doc = new Document(pdfDocument, pageSize);
+            Document doc = new Document(pdfDocument, PAGE_SIZE);
 
-            PdfCanvas canvas = new PdfCanvas(pdfDocument.getPage(1));
-            canvas.addImageFittedIntoRectangle(ImageDataFactory.create(backgroundBytes), pageSize, true);
+            int pageCount = pdfDocument.getNumberOfPages();
+            for (int pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
+                processPage(pdfDocument.getPage(pageNumber), backgroundBytes);
+            }
 
             doc.close();
-        } catch (Exception ex) {
+        } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    private static void processPage(PdfPage page, byte[] backgroundBytes) {
+        PdfCanvas canvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), page.getDocument());
+        canvas.addImageFittedIntoRectangle(ImageDataFactory.create(backgroundBytes), PAGE_SIZE, true);
+    }
+
+    private static String getOutputFilename(String[] args, String inputFilename) {
+        String outputFilename;
+        if (args.length > 1) {
+            outputFilename = args[1];
+        } else {
+            outputFilename = inputFilename.replace(".pdf", "-bg.pdf");
+        }
+        return outputFilename;
+    }
+
+    private static byte[] loadBackground() throws IOException {
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("2020-briefpapier.png")) {
+            if (inputStream == null) {
+                throw new IOException("File could not be opened");
+            }
+
+            return inputStream.readAllBytes();
         }
     }
 }
